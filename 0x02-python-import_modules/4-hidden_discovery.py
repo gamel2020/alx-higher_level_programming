d79384281b84f4f30dffb0af92a7725290e40ccf
gamel2020 <gamelnashaat2018@gmail.com>
2023-12-04 08:40:46 -0800

    py2

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """print all hidden dirctories"""
     import hidden_4
-
-
-    for i in dir(hidden_4):
-    if i[0:2] != "__":
-        print(i)
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)
